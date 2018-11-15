236d1b18cb0a488183726c8c5ef3278523da4dda
Max Gusenbauer <max.gusenb@gmail.com>
2018-11-15 10:11:52 +0100

    Added .ts Files to Repository

---
+++ app.js
@@ -0,0 +1,37 @@
+$("#submit").hide();
+$(document).on("ready change", function () {
+    var correct = true;
+    if ($("#firstName").val() == "") {
+        $("#firstNameMandatory").show();
+        correct = false;
+    }
+    else {
+        $("#firstNameMandatory").hide();
+    }
+    if ($("#lastName").val() == "") {
+        $("#lastNameMandatory").show();
+        correct = false;
+    }
+    else {
+        $("#lastNameMandatory").hide();
+    }
+    if ($("#email").val() == "" && $("#newsletter").prop('checked')) {
+        $("#emailMandatory").hide();
+    }
+    else {
+        $("#emailMandatory").show();
+        correct = false;
+    }
+    if ($("#mediaChannelSelect").val() == "Other") {
+        $("#otherMediaChannel").show();
+    }
+    else {
+        $("#otherMediaChannel").hide();
+    }
+    if (correct == false) {
+        $("#submit").hide();
+    }
+    else {
+        $("#submit").show();
+    }
+});
